93cce341832f0bc4f723921e79d308411fb3a1ac
alicia <team3229@robpress.ecs.soton.ac.uk>
2026-01-22 08:43:25 +0000

    handle keys

--- game.js
+++ game.js
@@ -29,4 +29,24 @@ function updateTile(tile, num){
     }
 }
 
+addEventListener('keyup', (e) => {
+    switch (e.key){
+        case 'ArrowUp':
+            console.log("up")
+            break
+        case 'ArrowDown':
+            console.log("down")
+            break
+        case  'ArrowLeft':
+            console.log("left")
+            break
+        case 'ArrowRight':
+            console.log("right")
+            break   
+        default : 
+            break
+    }
+})
+
+
 startGame()
